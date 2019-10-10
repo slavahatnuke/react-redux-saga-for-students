fdce1d287e21b66d5d2164d396abd28253a63059
slava hatnuke <slava.hatnuke@gmail.com>
2019-10-11 00:37:39 +0300

    - router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="container">
-     <div className="row">
-       <div className="col">
-         1
-       </div>
-     </div>
-    </div>
+    <Router>
+      <div className="container">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">home</Link>
+            </li>
+            <li>
+              <Link to="/1">Page 1</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/1">
+            page 1
+          </Route>
+          <Route path="/">
+            <div className="row">
+              <div className="col">
+                home
+              </div>
+            </div>
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
