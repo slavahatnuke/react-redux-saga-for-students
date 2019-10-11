e8ab684dd83b0d643677d609fcc524eddbbe9b81
slava hatnuke <slava.hatnuke@gmail.com>
2019-10-11 03:19:03 +0300

    - extracted action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
-import {GET_NEWS} from '../types';
+import { GET_NEWS, NEWS_RECEIVED } from '../types';
 
 export const getNews = () => ({
   type: GET_NEWS,
+});
+
+export const receivedNews = ({ articles }) => ({
+  type: NEWS_RECEIVED,
+  articles,
 });
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,15 +1,14 @@
 import { put, takeLatest, all } from 'redux-saga/effects';
 
 import { GET_NEWS, NEWS_RECEIVED } from '../types';
+import { receivedNews } from '../actions';
 
 function* fetchNews() {
   const response = yield fetch('https://newsapi.org/v1/articles?source=cnn&apiKey=c39a26d9c12f48dba2a5c00e35684ecc')
   const json = yield response.json();
+  const { articles } = json;
 
-  yield put({
-    type: NEWS_RECEIVED,
-    articles: json.articles,
-  });
+  yield put(receivedNews({ articles }));
 }
 
 function* actionWatcher() {
